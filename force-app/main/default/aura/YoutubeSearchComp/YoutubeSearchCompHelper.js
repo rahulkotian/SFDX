72d1046688ba838b3c1be91f8625f46ba9958ccb
rahulkotian <rahul.kotian@yahoo.com>
2019-01-28 15:42:05 -0600

    test

--- force-app/main/default/aura/YoutubeSearchComp/YoutubeSearchCompHelper.js
+++ force-app/main/default/aura/YoutubeSearchComp/YoutubeSearchCompHelper.js
@@ -1,17 +1,8 @@
-({
-    setSearchTerm: function (component, event) {
-        var searchTerm = component.find('searchBox').getElement().value;
-        console.log(searchTerm); component.set("v.searchTerm", searchTerm);
-        // create a one-time use instance of the search action      
-        //   // in the server-side controller      
-        var action = component.get("c.search");
-        action.setParams({ searchstr: searchTerm });
-        action.setCallback(this, function (response) {
-            var state = response.getState();
-            if (state === "SUCCESS") {
-                var youtube_res = JSON.parse(response.getReturnValue()); console.log(response.getReturnValue()); console.log(youtube_res); component.set("v.data", youtube_res);
-            } else if (state === "INCOMPLETE") {               // do something      
-            } else if (state === "ERROR") { var errors = response.getError(); if (errors) { if (errors[0] && errors[0].message) { console.log("Error message: " + errors[0].message); } } else { console.log("Unknown error"); } }
-        }); $A.enqueueAction(action);
+({  // Your RENDERER method overrides go here  
+    render: function (component, helper) {
+        var ret = this.superRender();        // do custom rendering here    
+            console.log('Inside render Function');  
+                  console.log('step2');    
+                      console.log('DOM ELEMENT IN render' +component.find("example").getElement());        console.log(ret);        return ret;  },        rerender : function(cmp, helper){      this.superRerender();      // do custom rerendering here  },        afterRender: function (component, helper) {      this.superAfterRender();        console.log('Inside afterRender Function');        console.log('step3');        console.log('COMPONENT IN afterrender' +component.find("example"));        console.log('DOM ELEMENT IN afterRender' +component.find("example").getElement());      // interact with the DOM here  },        unrender: function () {      this.superUnrender();      // do custom unrendering here  
     }
 })
